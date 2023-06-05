0eefa7fd0495b183e89a325bc3cb931723f94fee
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-06-05 17:19:06 +0600

    Setup nextra

---
+++ theme.config.tsx
@@ -0,0 +1,10 @@
+const themeConfig = {
+  logo: <span>Aidos Kanapyanov </span>,
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  search: {
+    placeholder: "Search in blog…",
+  },
+};
+export default themeConfig;
