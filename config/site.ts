12f5a42aae8dbd8526845c98d38b21cac62e12f0
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-06-06 15:05:37 +0600

    Define social links

---
+++ config/site.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  links: {
+    github: "https://github.com/aidoskanapyanov",
+    linkedin: "https://www.linkedin.com/in/aidos-kanapyanov",
+    discord: "https://discordapp.com/users/898420127731712001",
+    telegram: "https://t.me/aidos_k",
+  },
+};
