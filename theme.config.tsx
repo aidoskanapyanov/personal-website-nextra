3eba21f6e31de25ddad2689398a3643da3c6ddd9
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-06-06 14:53:58 +0600

    Use custom icons for project, chat

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { Icons } from "./components/icons";
 
 const themeConfig = {
   logo: <span>Aidos Kanapyanov</span>,
@@ -8,17 +9,11 @@ const themeConfig = {
   },
   project: {
     link: "https://github.com/aidoskanapyanov",
+    icon: <Icons.github dim={false} />,
   },
   chat: {
     link: "https://www.linkedin.com/in/aidos-kanapyanov/",
-    icon: (
-      <svg width="24" height="24">
-        <path
-          fill="currentColor"
-          d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"
-        />
-      </svg>
-    ),
+    icon: <Icons.linkedin dim={false} />,
   },
   useNextSeoProps() {
     const { asPath } = useRouter();
