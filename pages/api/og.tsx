58cd4b1dafd1483dc3a9c2ca95d6bb9615fbe6f1
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-06-08 18:40:06 +0600

    Enable edge runtime for dynamic og:image generation

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,9 +1,9 @@
 import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
 
-// export const config = {
-//   runtime: "edge",
-// };
+export const config = {
+  runtime: "edge",
+};
 
 export default function handler(request: NextRequest) {
   try {
