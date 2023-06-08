8f1d70db7cbbaead9d2b2293f92f2872e9394373
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-06-08 12:15:54 +0600

    Set custom divide width @ sidebar

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export default function Sidebar({}: Props) {
   return (
-    <div className="dark:text-white flex flex-col divide-y-2 py-10">
+    <div className="dark:text-white flex flex-col divide-y-[1px] py-10">
       <Profile />
       <AboutShort />
     </div>
