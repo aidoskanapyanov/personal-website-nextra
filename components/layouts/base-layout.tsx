41077f8e7cb17c73af234dfd62754ee28e9cefa5
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-06-08 12:35:22 +0600

    Remove base layout borders and paddings for sm devices

--- components/layouts/base-layout.tsx
+++ components/layouts/base-layout.tsx
@@ -8,11 +8,11 @@ type Props = {
 export default function BaseLayout({ children }: Props) {
   return (
     <div className="mt-6">
-      <div className="grid md:grid-cols-3">
+      <div className="grid grid-cols-1 md:grid-cols-3">
         <div className="md:mr-10 col-span-1 order-2 md:order-1">
           <Sidebar />
         </div>
-        <div className="p-6 pb-14 md:p-14 col-span-2 order-1 md:order-2 border dark:border-none rounded-lg dark:bg-[#1c1c1d]">
+        <div className="pb-10 md:p-12 col-span-2 order-1 md:order-2 border-b md:border md:dark:border-none md:rounded-lg md:dark:bg-[#1c1c1d]">
           {children}
         </div>
       </div>
