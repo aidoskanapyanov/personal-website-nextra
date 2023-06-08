4907359c91b7442be6ff0362010119879579d4f3
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-06-08 14:37:22 +0600

    Remove pb on sidebar

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export default function Sidebar({}: Props) {
   return (
-    <div className="dark:text-white flex flex-col divide-y-[1px] py-10">
+    <div className="dark:text-white flex flex-col divide-y-[1px] pt-10">
       <Profile />
       <AboutShort />
     </div>
