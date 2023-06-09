dec7dad7c32c6f7d185102708b7b26e9cff83016
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-06-09 15:58:19 +0600

    Update custom og generator edge function

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -5,61 +5,52 @@ export const config = {
   runtime: "edge",
 };
 
-export default function handler(request: NextRequest) {
+export default async function handler(req: NextRequest) {
   try {
-    const { searchParams } = new URL(request.url);
+    const { searchParams } = new URL(req.url);
 
-    // ?title=<title>
-    const hasTitle = searchParams.has("title");
-    const title = hasTitle
+    // dynamic params
+    const title = searchParams.has("title")
       ? searchParams.get("title")?.slice(0, 100)
       : "My default title";
+    const image = searchParams.get("image") || "";
+    const author = searchParams.get("author") || "Include a guest name";
+    const readTime = searchParams.get("readTime") || "0";
 
     return new ImageResponse(
       (
-        <div
-          style={{
-            backgroundColor: "black",
-            backgroundSize: "150px 150px",
-            height: "100%",
-            width: "100%",
-            display: "flex",
-            textAlign: "center",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-            flexWrap: "nowrap",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              justifyItems: "center",
-            }}
-          >
-            <img
-              alt="Vercel"
-              height={200}
-              src="data:image/svg+xml,%3Csvg width='116' height='100' fill='white' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M57.5 0L115 100H0L57.5 0z' /%3E%3C/svg%3E"
-              style={{ margin: "0 30px" }}
-              width={232}
-            />
-          </div>
-          <div
-            style={{
-              fontSize: 60,
-              fontStyle: "normal",
-              letterSpacing: "-0.025em",
-              color: "white",
-              marginTop: 30,
-              padding: "0 120px",
-              lineHeight: 1.4,
-              whiteSpace: "pre-wrap",
-            }}
-          >
-            {title}
+        <div tw="h-full w-full flex items-start justify-start bg-slate-300 p-20">
+          <div tw="flex h-full items-center w-full">
+            <div tw="flex-1 flex flex-col mr-20">
+              <h1 tw="text-7xl">{title}</h1>
+              <p tw="text-slate-800 text-xl mt-0">
+                {author} • {readTime}min
+              </p>
+            </div>
+            {image ? (
+              <div tw="flex relative">
+                <svg
+                  tw="absolute top-[-300px] left-[-100px] opacity-20"
+                  id="visual"
+                  viewBox="0 0 900 600"
+                  width="900"
+                  height="600"
+                  version="1.1"
+                >
+                  <g transform="translate(444.3593826782917 273.8643784322123)">
+                    <path
+                      fill="#0f172a"
+                      d="M186.1 -166.4C230.8 -141.4 249.4 -70.7 237.7 -11.7C226 47.4 184.1 94.8 139.4 139.9C94.8 185.1 47.4 228 -2.2 230.3C-51.9 232.5 -103.7 194 -149.2 148.9C-194.7 103.7 -233.9 51.9 -229.5 4.4C-225.1 -43.1 -177.3 -86.3 -131.8 -111.3C-86.3 -136.3 -43.1 -143.1 13.8 -156.9C70.7 -170.7 141.4 -191.4 186.1 -166.4"
+                    ></path>
+                  </g>
+                </svg>
+                <img
+                  style={{ objectFit: "cover" }}
+                  tw="mx-auto border-8 border-gray-800 w-[300px] h-[300px] rounded-full"
+                  src={image}
+                />
+              </div>
+            ) : null}
           </div>
         </div>
       ),
