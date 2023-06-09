c37fce16f73f6702ea5f18d32377bb5a45a30e83
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-06-09 15:59:43 +0600

    Try using experimental edge

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
 
 export const config = {
-  runtime: "edge",
+  runtime: "experimental-edge",
 };
 
 export default async function handler(req: NextRequest) {
