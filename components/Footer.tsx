f8645a50a66885d0654f873aae971a3936b1bbf3
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-06-09 12:49:16 +0600

    Make separate component for FooterIcons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,41 @@ import { ThemeSwitch } from "nextra-theme-docs";
 import { ReactElement } from "react";
 import { Icons } from "./icons";
 
+export function FooterIcons() {
+  return (
+    <>
+      <Link
+        target="_blank"
+        href={siteConfig.links.github}
+        className="items-center space-x-2 md:flex"
+      >
+        <Icons.github />
+      </Link>
+      <Link
+        target="_blank"
+        href={siteConfig.links.linkedin}
+        className="items-center space-x-2 md:flex"
+      >
+        <Icons.linkedin />
+      </Link>
+      <Link
+        target="_blank"
+        href={siteConfig.links.discord}
+        className="items-center space-x-2 md:flex"
+      >
+        <Icons.discord />
+      </Link>
+      <Link
+        target="_blank"
+        href={siteConfig.links.telegram}
+        className="items-center space-x-2 md:flex"
+      >
+        <Icons.telegram />
+      </Link>
+    </>
+  );
+}
+
 export function FooterContent() {
   return (
     <>
@@ -13,34 +48,7 @@ export function FooterContent() {
       </h2>
       <div className="w-full flex flex-col gap-4 md:flex-row md:justify-between justify-center items-center">
         <div className="flex justify-center items-center gap-4">
-          <Link
-            target="_blank"
-            href={siteConfig.links.github}
-            className="items-center space-x-2 md:flex"
-          >
-            <Icons.github />
-          </Link>
-          <Link
-            target="_blank"
-            href={siteConfig.links.linkedin}
-            className="items-center space-x-2 md:flex"
-          >
-            <Icons.linkedin />
-          </Link>
-          <Link
-            target="_blank"
-            href={siteConfig.links.discord}
-            className="items-center space-x-2 md:flex"
-          >
-            <Icons.discord />
-          </Link>
-          <Link
-            target="_blank"
-            href={siteConfig.links.telegram}
-            className="items-center space-x-2 md:flex"
-          >
-            <Icons.telegram />
-          </Link>
+          <FooterIcons />
         </div>
         <div className="flex flex-col gap-2 justify-center items-center">
           <p className="text-xs md:text-base text-gray-500 dark:text-[#888888]">
